5a3bd32161ee3a2ff7a3d5700c28834c4eb4a521
김성국 <live8681@naver.com>
2021-08-12 11:47:36 +0900

    푸쉬 테스트

--- JenkinsWinformTest/Form1.Designer.cs
+++ JenkinsWinformTest/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace JenkinsWinformTest
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -37,15 +38,26 @@ namespace JenkinsWinformTest
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(416, 192);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.Size = new System.Drawing.Size(69, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "푸쉬 테스트";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +69,7 @@ namespace JenkinsWinformTest
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- JenkinsWinformTest/Form1.cs
+++ JenkinsWinformTest/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,27 @@ namespace JenkinsWinformTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("업데이트를 하시겠습니까?", "알림", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Update();
+            }
+        }
+
+        /// <summary>
+        /// 업데이트
+        /// </summary>
+        /// <param name="p"></param>
+        private new void Update()
+        {
+            string updateAppPath = string.Format(@"{0}\UpdateManager.exe", Application.StartupPath);
+            Process.Start(updateAppPath);
+
+            this.Dispose();
         }
     }
 }
